cca05b68b0a039c4e5fa6b6f5263bffb807ff924
Leon de Rijke <leon@cmsworks.nl>
2012-09-02 16:56:05 +0200

    Don't print all results

--- exitwp.py
+++ exitwp.py
@@ -98,7 +98,7 @@ def parse_wp_xml(file):
                     tag = q
                 try:
                     result = i.find(ns[namespace] + tag).text
-                    print result
+                    #print result
                 except AttributeError:
                     result = "No Content Found"
                 if unicode_wrap:
